fba91557539f0d34bb2e326558b93f044826f895
rethink9268 <108924508+rethink9268@users.noreply.github.com>
2023-02-01 14:29:53 +0700

    hello

--- src/pages/Products/ListView.js
+++ src/pages/Products/ListView.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
 export default function ListView({ products }) {
-    console.log(products);
+
+    console.log(products.name);
     return (
         <div>ListView</div>
     )
